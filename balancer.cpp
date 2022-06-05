916c834cac828ed55246aa9911a43ccefb995b23
Ivan Vyborov <94543298+Xom9KP@users.noreply.github.com>
2022-06-05 06:01:39 +0300

    Create balancer.cpp

---
+++ balancer.cpp
@@ -0,0 +1,303 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <cassert>
+#include <chrono>
+#include <random>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+//
+//#include <ext/pb_ds/assoc_container.hpp>
+//#define unordered_map gp_hash_table
+//using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define pa pair<int8_t, ull> 
+#define pac pair<char, ull> 
+#define pam pair<ll, unordered_map<ull, pac, chash>> 
+
+using namespace std;
+using __team__ = vector<string>;
+
+class DataBase {
+private:
+	unordered_map<string, vector<int>> _points;
+	unordered_map<string, int> _indByName;
+	int _cntParametrs = 9;
+
+public:
+	// Прием		Подача		    Удар			         Блок	
+	// Довод Cкор	Сила Попадан	Сила Попадан Высота	     Высота	КПД
+	DataBase() {
+		_points["Женя"] = { 5, 2, 7, 8, 5, 7, 2, 1, 2 };
+		_points["Леша"] = { 7, 8, 8, 7, 9, 8, 8, 7, 8 };
+		_points["Ваня"] = { 7, 8, 8, 7, 8, 8, 7, 6, 8 };
+		_points["Дима"] = { 7, 8, 6, 4, 6, 3, 2, 1, 1 };
+		_points["Степа"] = { 6, 7, 7, 4, 7, 4, 4, 3, 4 };
+		_points["Алина"] = { 5, 5, 5, 4, 4, 4, 1, 0, 1 };
+		_points["Тема"] = { 6, 6, 6, 6, 6, 5, 7, 6, 8 };
+		_points["Рома"] = { 6, 7, 8, 5, 9, 6, 10, 9, 7 };
+		_points["Коля"] = { 6, 6, 6, 6, 7, 5, 7, 6, 5 };
+		_points["Радион"] = { 5, 6, 8, 4, 8, 5, 7, 6, 7 };
+		_points["Аня"] = { 6, 4, 4, 4, 3, 4, 0, 0, 0 };
+		_points["Ира"] = { 3, 4, 3, 5, 3, 3, 0, 0, 0 };
+		_points["Настя"] = { 5, 5, 5, 5, 4, 4, 3, 2, 1 };
+		_points["Серега"] = { 6, 5, 6, 5, 5, 5, 4, 3, 1 };
+		_points["Гоша"] = { 7, 6, 6, 7, 6, 6, 6, 5, 3 };
+		_points["Леся"] = { 6, 5, 6, 6, 4, 5, 1, 0, 1 };
+		_points["Саня"] = { 8, 7, 8, 7, 7, 7, 4, 3, 4 };
+		_points["Катя"] = { 5, 6, 6, 8, 5, 5, 2, 1, 1 };
+		_points["Саша"] = { 2, 4, 3, 4, 3, 3, 3, 2, 2 };
+		_points["Маша"] = { 4, 4, 5, 4, 3, 3, 0, 0, 0 };
+		_points["Артем"] = { 3, 4, 5, 6, 5, 5, 4, 3, 5 };
+		_points["Вадим"] = { 6, 6, 6, 7, 6, 5, 4, 3, 4 };
+
+		_indByName["Женя"] = 0;
+		_indByName["Леша"] = 1;
+		_indByName["Ваня"] = 2;
+		_indByName["Дима"] = 3;
+		_indByName["Степа"] = 4;
+		_indByName["Алина"] = 5;
+		_indByName["Тема"] = 6;
+		_indByName["Рома"] = 7;
+		_indByName["Коля"] = 8;
+		_indByName["Радион"] = 9;
+		_indByName["Аня"] = 10;
+		_indByName["Ира"] = 11;
+		_indByName["Настя"] = 12;
+		_indByName["Серега"] = 13;
+		_indByName["Гоша"] = 14;
+		_indByName["Леся"] = 15;
+		_indByName["Саня"] = 16;
+		_indByName["Катя"] = 17;
+		_indByName["Саша"] = 18;
+		_indByName["Маша"] = 19;
+		_indByName["Артем"] = 20;
+		_indByName["Вадим"] = 21;
+	}
+
+	void setPoints(const string& name, const vector<int> value) {
+		_points[name] = value;
+	};
+
+	unordered_map<string, vector<int>> getPoints() {
+		return _points;
+	}
+
+	int getCntParametrs() {
+		return _cntParametrs;
+	}
+
+	vector<int> getPoints(const string& name) {
+		auto it = _points.find(name);
+		if (it == _points.end()) return vector<int>(_cntParametrs, -1);
+		return it->second;
+	}
+
+	int getCntMembers() {
+		return _points.size();
+	}
+
+	int getInd(const string& name) {
+		return _indByName[name];
+	}
+};
+
+class Members {
+private:
+	DataBase _db;
+	__team__ _numName;
+
+public:
+
+	Members(): _db(DataBase()) {
+		_numName = { "Женя", "Леша", "Ваня", "Дима", "Степа", "Алина", "Тема", "Рома", "Коля", "Радион",
+					"Аня", "Ира", "Настя", "Серега", "Гоша", "Леся", "Саня", "Катя", "Саша", "Маша", "Артем", "Вадим" };
+	};
+	
+	void setPoints(const string& name, const vector<int> value) {
+		_db.setPoints(name, value);
+	};
+
+	void setPoints(const int ind, const vector<int> value) {
+		setPoints(_numName[ind], value);
+	};
+
+	int getCntParametrs() {
+		return _db.getCntParametrs();
+	}
+
+	vector<int> getPoints(const string& name) {
+		return _db.getPoints(name);
+	}
+
+	vector<int> getPoints(const int ind) {
+		return getPoints(_numName[ind]);
+	}
+
+	int getCntMembers() {
+		return _db.getCntMembers();
+	}
+
+	string getName(const int ind) {
+		return _numName[ind];
+	}
+
+	int getInd(const string& name) {
+		return _db.getInd(name);
+	}
+};
+
+class Balance {
+private:
+	Members _members;
+	__team__ _visited;
+	int _cntTeams;
+	const int _cntPeopleInTeam = 6;
+	vector <__team__> _teams;
+
+public:
+	Balance(const __team__ visited, const int cntTeams) : _visited(visited), _cntTeams(cntTeams) {};
+
+	vector<int> calcTeamPower(const __team__& team) {
+		vector<int> teamPower(_members.getCntParametrs(), 0);
+		for (auto& it : team) {
+			vector<int> peoplePower = _members.getPoints(it);
+			for (int i = 0; i < teamPower.size(); ++i) {
+				teamPower[i] += peoplePower[i];
+			}
+		}
+
+		return teamPower;
+	}
+
+	bool isBalancedTeams(const __team__& first, const __team__& second, int delta = 0) {
+		auto firstPower = calcTeamPower(first);
+		auto secondPower = calcTeamPower(second);
+		int sumDelta = 0;
+		for (int i = 0; i < _members.getCntParametrs(); ++i) {
+			sumDelta += abs(firstPower[i] - secondPower[i]);
+		}
+
+		return sumDelta <= delta;
+	}
+
+	void constructUsed(vector<bool>& used) {
+		used.assign(_members.getCntMembers(), true);
+		for (auto& it : _visited) {
+			used[_members.getInd(it)] = false;
+		}
+	}
+
+	__team__ buildTeam(vector<bool>& used) {
+		__team__ currentTeam;
+		int cntMembers = _visited.size();
+		int cntPeople = _members.getCntMembers();
+		while (currentTeam.size() < _cntPeopleInTeam) {
+			int numberOfPeople = rand() % cntPeople;
+			if (!used[numberOfPeople]){
+				currentTeam.push_back(_members.getName(numberOfPeople));
+				used[numberOfPeople] = true;
+			}
+		}
+
+		return currentTeam;
+	}
+
+	void constructTeams() {
+		int cntMembers = _visited.size();
+		vector<bool> used;
+		constructUsed(used);
+		_teams.assign(_cntTeams, __team__());
+		for (int i = 0; i < _cntTeams; ++i) {
+			_teams[i] = buildTeam(used);
+		}
+	}
+
+
+	void balanceTeams() {
+		int cntTry = 0;
+		while (true) {
+			constructTeams();
+			if (isGoodBalance(cntTry / 1000)) {
+				printTeams();
+				return;
+			}
+			++cntTry;
+		}
+	}
+
+	bool isGoodBalance(int delta = 0) {
+		for (int i = 0; i < _cntTeams; ++i) {
+			for (int j = 0; j < _cntTeams; ++j) {
+				if (!isBalancedTeams(_teams[i], _teams[j], delta))
+					return false;
+			}
+		}
+
+		return true;
+	}
+
+	void printTeams() {
+		auto firstPower = calcTeamPower(_teams[0]);
+		auto secondPower = calcTeamPower(_teams[1]);
+		auto thirdPower = calcTeamPower(_teams[2]);
+
+		for (auto it : firstPower) {
+			cout << it << ", ";
+		}
+		cout << '\n';
+
+		for (auto it : secondPower) {
+			cout << it << ", ";
+		}
+		cout << '\n';
+
+		for (auto it : thirdPower) {
+			cout << it << ", ";
+		}
+		cout << '\n';
+
+		for (int i = 0; i < _cntPeopleInTeam; ++i) {
+			for (int j = 0; j < _cntTeams; ++j) {
+				cout << setw(20) << _teams[j][i];
+			}
+			cout << '\n';
+		}
+	}
+};
+
+__team__ readVisiters(const int cntVisited) {
+	__team__ visited(cntVisited);
+	for (auto& it : visited) {
+		cin >> it;
+	}
+
+	return visited;
+}
+
+signed main() {
+	std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+	srand(time(0));
+	setlocale(LC_ALL, "Russian");
+
+	int cntVisited;
+	cin >> cntVisited;
+	__team__ visited = readVisiters(cntVisited);
+
+	Balance T(visited, 3);
+	T.balanceTeams();
+}
